d7aa6f38f2db08fcb57bba174594bff8d13fcdef
Guilherme Moura <mouraggui@gmail.com>
2020-06-26 09:43:48 -0300

    piano config

--- src/js/Piano.js
+++ src/js/Piano.js
@@ -1,63 +1,88 @@
+import pagemap from 'pagemap';
+import { notes } from './notes';
 import { isTouchDevice } from './util';
 
-export default class Piano {
-  constructor (config = {}) {
-    if (!config.notes) {
-      throw new Error('Passing the config.notes parameter is mandatory');
-    }
+const $keyboardCotainer = document.querySelector('.piano__keyboard-container');
+const $btnPrev = document.querySelector('.piano__dashboard__btn-prev');
+const $btnNext = document.querySelector('.piano__dashboard__btn-next');
 
-    this.notes = config.notes;
-  }
+const playNote = (note) => {
+  if (!notes[note]) return;
 
-  playNote (note) {
-    if (!this.notes[note]) {
-      return;
-    }
+  notes[note].play();
+}
 
-    this.notes[note].play();
-  }
+const initMinimap = () => {
+  pagemap(document.querySelector('#map'), {
+    viewport: document.querySelector('.piano__keyboard-container'),
+    back: 'rgb(0,0,0)',
+    view: 'rgba(0,0,0,0.5)',
+    drag: 'rgba(0,0,0,0.5)',
+    styles: {
+      '.piano__keyboard__key--white': '#fff',
+      '.piano__keyboard__key--black': '#000',
+    },
+  });
+};
 
-  addKeyboardEvents () {
-    window.addEventListener('keydown', (event) => {
-      const $pianoKey = document.querySelector(`[data-key-code='${event.which}']`);
-
-      if (!$pianoKey || event.repeat) {
-        return;
-      }
-
-      const note = $pianoKey.getAttribute('data-note');
-      $pianoKey.classList.add('active');
-      this.playNote(note);
+const initHandlers = () => {
+  const preventHandler = event => event.preventDefault();
+  const getScrollHandler = fat => event => {
+    $keyboardCotainer.scrollTo({
+      top: 0,
+      left: $keyboardCotainer.scrollLeft + fat,
+      behavior: 'smooth',
     });
-
-    window.addEventListener('keyup', (event) => {
-      const $pianoKey = document.querySelector(`[data-key-code='${event.which}']`);
-
-      if (!$pianoKey) {
-        return;
-      }
-
-      $pianoKey.classList.remove('active');
-    });
-  }
-  
-  addClickEvents () {
-    document.querySelectorAll('[data-key-code]').forEach((key) => {
-      const handler = (event) => {
-        const note = key.getAttribute('data-note');
-        this.playNote(note);
-      };
-    
-      if (isTouchDevice()) {
-        key.addEventListener('touchstart', handler);
-      } else {
-        key.addEventListener('mousedown', handler);
-      }
-    });
-  }
-  
-  install () {
-    this.addClickEvents();
-    this.addKeyboardEvents();
   };
+
+  $btnPrev.addEventListener('click', getScrollHandler(-500));
+  $btnNext.addEventListener('click', getScrollHandler(500));
+  $keyboardCotainer.addEventListener('touchmove', preventHandler);
+  $keyboardCotainer.addEventListener('DOMMouseScroll', preventHandler);
+};
+
+const addKeyboardEvents = () => {
+  window.addEventListener('keydown', (event) => {
+    const $pianoKey = document.querySelector(`[data-key-code='${event.which}']`);
+
+    if (!$pianoKey) return;
+
+    const note = $pianoKey.getAttribute('data-note');
+    $pianoKey.classList.add('active');
+    playNote(note);
+  });
+
+  window.addEventListener('keyup', (event) => {
+    const $pianoKey = document.querySelector(`[data-key-code='${event.which}']`);
+
+    if (!$pianoKey) return;
+
+    $pianoKey.classList.remove('active');
+  });
+}
+  
+const addClickEvents = () => {
+  document.querySelectorAll('[data-key-code]').forEach((key) => {
+    const handler = () => {
+      const note = key.getAttribute('data-note');
+      playNote(note);
+    };
+  
+    if (isTouchDevice()) {
+      key.addEventListener('touchstart', handler);
+    } else {
+      key.addEventListener('mousedown', handler);
+    }
+  });
+}
+
+const install = () => {
+  initMinimap();
+  initHandlers();
+  addClickEvents();
+  addKeyboardEvents();
+}
+
+export {
+  install,
 };
