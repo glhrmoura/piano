8918c8057bf73ea75038b18cb4d702cdbb5d0ead
Guilherme Moura <glhrmoura@gmail.com>
2025-08-09 12:32:25 -0300

    feat: configure absolute paths with @ alias for imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
+  plugins: [react()],
   root: 'src',
   publicDir: '../public',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 })
