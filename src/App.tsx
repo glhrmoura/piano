15f9e28a081220cec40753eb3d1755147be1258e
Guilherme Moura <glhrmoura@gmail.com>
2025-08-09 12:33:06 -0300

    feat: create react app entry point and main component

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Piano } from '@/components/Piano';
+import '@/scss/index.scss';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <Piano />
+    </div>
+  );
+};
+
+export default App;
